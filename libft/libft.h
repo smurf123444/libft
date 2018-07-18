128e08a3286f907e590c0f0558246702308d4bcc
noob <Your Name>
2018-07-17 17:08:48 -0700

    Fixed atoi, memcpy, strncat and header

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: chmannin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 22:05:22 by chmannin          #+#    #+#             */
-/*   Updated: 2018/07/17 15:51:47 by chmannin         ###   ########.fr       */
+/*   Updated: 2018/07/19 12:39:45 by chmannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-//# define BUFF_SIZE 100 // checking to see if i need this
-//# define MALLCHECK(x) if (!x) return(-1); // checking to see what this does and if i need this
-//# define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\r' || x == '\f') // would be nice to keep but if i didnt call // this, it would look bad
+typedef struct 		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list *next;
+}						t_list;
 
 int					ft_isalpha(char c);
 int					ft_atoi(const char *str);
 int					ft_isblank(int c);
 int					ft_isspace(int c);
+void				ft_bzero(void *s, size_t n);
 size_t			ft_strlen(const char *s);
 int					ft_isprint(int c);
 int					ft_max(int x, int y);
@@ -32,7 +36,7 @@ int					ft_isascii(int chr);
 char					*ft_itoa(int n);
 int					ft_isalnum(int chr);
 int					ft_isdigit(int c);
-char				ft_strnew(size_t size);
+char					*ft_strnew(size_t size);
 void					*ft_memset(void *dst, int data, size_t len);
 void					*ft_memchr(const void *src, int c, size_t len);
 int					ft_memcmp(const void *s1, const void *s2, size_t len);
@@ -48,5 +52,9 @@ char					*ft_strrchr(const char *s, int c);
 char					*ft_strstr(const char *s1, const char *s2);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+void					ft_strrev(char *s);
+char					*ft_strcpy(char *dst, const char *src);
+void					*ft_memcpy(void *b, const void *c, size_t len);
+void					*ft_memccpy(void *
 
 #endif
